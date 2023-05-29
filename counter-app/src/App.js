e6308032917b95adff7590a3506fe2a88b616432
Stepan Omelyanenko (Desktop) <ose.26@mail.ru>
2023-05-29 21:31:43 +0300

    5. add check functions

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -16,21 +16,29 @@ function App() {
   const resetBtn = document.querySelector('.form__reset-button');
 
   const CheckCalculationAvailable = () => {
-    return false;
-  }
+    if (gender && age && height && weight && activity) {
+      setIsCalculationAvailable(true);
+    } else {
+      setIsCalculationAvailable(false);
+    }
+  };
 
   const CheckResetAvailable = () => {
-    return false;
-  }
+    if (gender || age || height || weight || activity) {
+      setIsResultAvailable(true);
+    } else {
+      setIsResultAvailable(false);
+    }
+  };
 
-  const [isCalculationAvailable, setIsCalculationAvailable] = useState([false]);
-  const [isResetAvailable, setIsResetAvailable] = useState([false]);
-  const [isResultAvailable, setIsResultAvailable] = useState([false]);
+  const [isCalculationAvailable, setIsCalculationAvailable] = useState(false);
+  const [isResetAvailable, setIsResetAvailable] = useState(false);
+  const [isResultAvailable, setIsResultAvailable] = useState(false);
 
   const [gender, setGender] = useState('male');
-  const [age, setAge] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
+  const [age, setAge] = useState(null);
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
   const [activity, setActivity] = useState('min');
 
   const genderChangeHandle = (evt) => {
@@ -205,14 +213,14 @@ function App() {
                 <button className="form__submit-button button" name="submit" type="submit" disabled>
                   Рассчитать
                 </button>
-                <button className="form__reset-button" name="reset" type="reset" disabled>
+                <button className="form__reset-button" name="reset" type="reset" disabled={!isResetAvailable}>
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="#FD3636" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd"
                           d="M13.4143 12.0002L18.7072 6.70725C19.0982 6.31625 19.0982 5.68425 18.7072 5.29325C18.3162 4.90225 17.6842 4.90225 17.2933 5.29325L12.0002 10.5862L6.70725 5.29325C6.31625 4.90225 5.68425 4.90225 5.29325 5.29325C4.90225 5.68425 4.90225 6.31625 5.29325 6.70725L10.5862 12.0002L5.29325 17.2933C4.90225 17.6842 4.90225 18.3162 5.29325 18.7072C5.48825 18.9022 5.74425 19.0002 6.00025 19.0002C6.25625 19.0002 6.51225 18.9022 6.70725 18.7072L12.0002 13.4143L17.2933 18.7072C17.4882 18.9022 17.7443 19.0002 18.0002 19.0002C18.2562 19.0002 18.5122 18.9022 18.7072 18.7072C19.0982 18.3162 19.0982 17.6842 18.7072 17.2933L13.4143 12.0002Z"/>
                   </svg>
                   <span>
-                Очистить поля и расчёт
-              </span>
+                    Очистить поля и расчёт
+                  </span>
                 </button>
               </div>
             </form>
